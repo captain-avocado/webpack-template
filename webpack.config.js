e4f26d0c5b4bbe17c9ca5e6bcb2465597da7f20c
Artem Kaloev <kaloev.eltech@gmail.com>
2018-03-11 15:39:57 +0300

    updated scss-modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,51 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+const sass = require('./webpack/sass');
 
 const paths = {
     src: path.join(__dirname, 'src/'),
     dist: path.join(__dirname, 'dist/')
 };
 
-const common = {
-    entry: {
-        'index': paths.src + 'pages/index/index.js',
-        'blog': paths.src + 'pages/blog/blog.js'
-    },
-    output: {
-        path: paths.dist,
-        filename: '[name].js'
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            filename: 'index.html',
-            chunks: ['index'],
-            template: paths.src + 'pages/index/index.pug'
-        }),
-        new htmlWebpackPlugin({
-            filename: 'blog.html',
-            chunks: ['blog'],
-            template: paths.src + 'pages/blog/blog.pug'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
-            }
+const common = merge([
+    {
+        entry: {
+            'index': paths.src + 'pages/index/index.js',
+            'blog': paths.src + 'pages/blog/blog.js'
+        },
+        output: {
+            path: paths.dist,
+            filename: '[name].js'
+        },
+        plugins: [
+            new htmlWebpackPlugin({
+                filename: 'index.html',
+                chunks: ['index'],
+                template: paths.src + 'pages/index/index.pug'
+            }),
+            new htmlWebpackPlugin({
+                filename: 'blog.html',
+                chunks: ['blog'],
+                template: paths.src + 'pages/blog/blog.pug'
+            })
         ]
     },
-    devServer: {
-        stats: 'errors-only'
-    }
-};
-
-const developmentConfig = {
-    devServer: {
-        stats: 'errors-only',
-        port: 9000
-    }
-};
+    pug()
+]);
 
 module.exports = function(env) {
     if (env === 'production') {
         return common;
     } 
     if (env === 'development') {
-        return Object.assign(
-            {},
+        return merge([
             common,
-            developmentConfig
-        )
+            devserver(),
+            sass()
+        ])
     }
-}
+};
